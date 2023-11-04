03f2709d9eb326747acb9c21753f12162d0b797c
Sabari7373 <sabariganesh7373@gmail.com>
2023-11-04 13:40:03 +0530

    App.use changed

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ const { db } = require("./models/leads");
 // Middleweare
 app.use(express.json());
 
-app.use(
-  cors({
-    orgin:"*",
-    methods:"GET,POST,PUT,DELETE",
-    credentials:true
-  })
-)
+app.use(cors())
 
 
 app.use("api/leads", leadsRoute) // for leads
